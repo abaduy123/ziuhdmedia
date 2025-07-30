0d087f62cb4e96ecd73cb985bb891a1a15a3bb2c
abaduy123 <dgikpp@gmail.com>
2025-07-30 20:33:37 +0300

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from "@/components/hero"
 import { Services } from "@/components/services"
 import { About } from "@/components/about"
 import { Stats } from "@/components/stats"
-import { Testimonials } from "@/components/testimonials"
+
 import { Contact } from "@/components/contact"
 import { Footer } from "@/components/footer"
 
